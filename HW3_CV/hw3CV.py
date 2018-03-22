7ef03dfa3664409401bf7585491c733926792a44
jlim2 <jlim2@macalester.edu>
2018-03-22 16:28:23 -0500

    Working on computing ORB for each alphalphabet and saving results into a dictionary. Not working yet. Also added '.idea' to '.gitignore'

--- HW3_CV/hw3CV.py
+++ HW3_CV/hw3CV.py
@@ -9,6 +9,9 @@ This is for Homework 3: Computer Vision
 
 import cv2
 import numpy as np
+from os import listdir
+from os.path import isfile, join
+import os
 
 def identifyWords(img):
     """First, identify if a sign is in an image by looking (perhaps) for a white or black box, or a box of
@@ -74,7 +77,7 @@ def showHoughLines(img):
 
 def showORB(img):
     # Initiate STAR detection (http://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_feature2d/py_orb/py_orb.html) (https://stackoverflow.com/questions/32702433/opencv-orb-detector-finds-very-few-keypoints)
-    orb = cv2.ORB_create(nfeatures=1000, scoreType=cv2.ORB_FAST_SCORE)
+    orb = cv2.ORB_create(nfeatures=10000, scoreType=cv2.ORB_FAST_SCORE)
     keypoints = orb.detect(image=img)
 
     img2 = cv2.drawKeypoints(img, keypoints, None, color=(0, 255, 0))
@@ -89,7 +92,7 @@ def computeORB(img):
     :return: keypoints, des
     """
     # Initiate STAR detection (http://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_feature2d/py_orb/py_orb.html) (https://stackoverflow.com/questions/32702433/opencv-orb-detector-finds-very-few-keypoints)
-    orb = cv2.ORB_create(nfeatures=1000, scoreType=cv2.ORB_FAST_SCORE)
+    orb = cv2.ORB_create(nfeatures=10000, scoreType=cv2.ORB_FAST_SCORE)
     keypoints = orb.detect(image=img)
     keypoints, des = orb.compute(img, keypoints)
 
@@ -97,11 +100,29 @@ def computeORB(img):
 
 def readModelAlphabets(alphabetImgs):
     alphFeatureDict = {}
-    imgNames = []
-    for img in alphabetImgs:
-        imgName = img.strip(".png")
-        imgNames.append(imgName)
-    print(imgNames)
+    # imgNames = []
+    # for img in alphabetImgs:
+    #     imgName = img.strip(".png")
+    #     imgNames.append(imgName)
+    # print(imgNames)
+
+    imgFiles = [f for f in listdir("alphabets") if isfile(join("alphabets", f))]
+    # testImgFiles.append([f for f in listdir("TestImages/Letters") if isfile(join("TestImages/Letters", f))])
+
+    # imgNames = []
+    alphFeatureDict = {}
+    for imgFile in imgFiles:
+        if (imgFile.endswith('.png')):
+            imgName = imgFile.strip(".png")
+            # imgNames.append(imgName)
+            img = cv2.imread(imgFile)
+            # print(imgFile)
+            kp, des = computeORB(img)
+            print(kp)
+            print(des)
+            alphFeatureDict.update({imgName: (kp, des)})
+
+    return alphFeatureDict
 
 if __name__ == '__main__':
     img = cv2.imread('LetterInWhite.png', 0)
@@ -129,7 +150,13 @@ if __name__ == '__main__':
     # print(descriptors.shape)
     # for des in descriptors:
     #     pass
-    #     # print(des.shape)
-    #
+        # print(des.shape)
 
-    readModelAlphabets()
+
+    print(readModelAlphabets('alphabets'))
+    imgA = cv2.imread('alphabets/A.png')
+    showORB(imgA)
+    keypoints, descriptors = computeORB(imgA)
+    # print(des.shape)
+    # print(kp)
+    # print(des)
