cf74033facfd94393c76877413d260cdcddb4356
jlim2 <jlim2@macalester.edu>
2018-03-27 14:34:46 -0500

    ORB detection for letter samples working pretty well with nFeatures of ORB reduced to around 10.

--- HW3_CV/hw3CV.py
+++ HW3_CV/hw3CV.py
@@ -12,6 +12,8 @@ import numpy as np
 from os import listdir
 from os.path import isfile, join
 from collections import OrderedDict
+from PIL import Image
+# import pytesseract  #Python Tesseract; Installation required.
 import os
 
 def readImgFeatures(img):
@@ -78,7 +80,7 @@ def showHoughLines(img):
 
 def showORB(img):
     # Initiate STAR detection (http://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_feature2d/py_orb/py_orb.html) (https://stackoverflow.com/questions/32702433/opencv-orb-detector-finds-very-few-keypoints)
-    orb = cv2.ORB_create(nfeatures=10000, scoreType=cv2.ORB_FAST_SCORE)
+    orb = cv2.ORB_create(nfeatures=100000, scoreType=cv2.ORB_FAST_SCORE)
     keypoints = orb.detect(image=img)
 
     img2 = cv2.drawKeypoints(img, keypoints, None, color=(0, 255, 0))
@@ -94,7 +96,7 @@ def computeORB(img):
     """
     # Initiate STAR detection (http://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_feature2d/py_orb/py_orb.html) (https://stackoverflow.com/questions/32702433/opencv-orb-detector-finds-very-few-keypoints)
     # orb = cv2.ORB_create(nfeatures=10000, scoreType=cv2.ORB_FAST_SCORE)
-    orb = cv2.ORB_create()
+    orb = cv2.ORB_create(nfeatures=20, scoreType=cv2.ORB_FAST_SCORE)
 
     # keypoints = orb.detect(image=img)
     keypoints, des = orb.detectAndCompute(img, None)
@@ -181,11 +183,12 @@ if __name__ == '__main__':
     letterFeatureDict = readLetterFeatures('letterSamples')
     letterImgs = letterFeatureDict.keys()
 
-    # targetImgs = {}
-    # for img in alphImgs:
-    #     targetImg = cv2.imread(img)
-    #     kpTarget = alphFeatureDict.get(targetImg)[0]
-    #     desTarget = alphFeatureDict.get(targetImg)[1]
+    targetImgs = {}
+    for img in letterImgs:
+        targetImg = cv2.imread("letterSamples/"+img+".png")
+
+        kpTarget = letterFeatureDict.get(img)[0]
+        desTarget = letterFeatureDict.get(img)[1]
 
     #targetImg = cv2.imread('alphabets/A.png')
     #kpTarget = alphFeatureDict.get('A')[0]
@@ -193,77 +196,77 @@ if __name__ == '__main__':
 
 
 
-    """
-    vidCap = cv2.VideoCapture(0)
-    while True:
-        gotOne, frame = vidCap.read()
-        if (gotOne):
-            print("gotOne")
-            kpQuery, desQuery = computeORB(frame)
 
-            # for targetImg in alphImgs:
+    # vidCap = cv2.VideoCapture(0)
+    # while True:
+    #     gotOne, frame = vidCap.read()
+    #     if (gotOne):
+    #         print("gotOne")
+    #         kpQuery, desQuery = computeORB(frame)
+    #
+    #         # for targetImg in alphImgs:
+    #
+    #         matches = flanner.match(desTarget, desQuery)
+    #         matches.sort(key=lambda x: x.distance)  # sort by distance
+    #
+    #
+    #         matchedKPs = {}
+    #         # draw matches with distance less than threshold
+    #         #i = 0
+    #         #compare each alphImgs to frame and count how many matches
+    #         # for img in alphImgs:
+    #         #     desTarget = alphFeatureDict.get(img)[1]
+    #         #     matches = flanner.match(desTarget, desQuery)
+    #         #     for i in range(len(matches)):
+    #         #         if matches[i].distance > 50.0:
+    #         #             break
+    #         #         print(matches[i])
+    #
+    #
+    #         for alph in letterFeatureDict:
+    #             targetImg = cv2.imread('letterSamples/'+alph+'.png')
+    #             kpTarget = letterFeatureDict.get(alph)[0]
+    #             desTarget = letterFeatureDict.get(alph)[1]
+    #
+    #             matches = flanner.match(desTarget, desQuery)
+    #             matches.sort(key=lambda x: x.distance)  # sort by distance
+    #             numMatched = 0
+    #             for i in range(len(matches)):
+    #                 if matches[i].distance > 50.0:
+    #                     break
+    #                 numMatched += 1
+    #             matchedKPs.update({alph: numMatched})
+    #
+    #             # print("matches[i]: "+str(matches[i]), end=" ")
+    #             # print("distance: "+str(matches[i].distance), end=" ")
+    #             # print("imgIdx: "+str(matches[i].imgIdx), end=" ")
+    #             # print("queryIdx: " + str(matches[i].queryIdx), end=" ")
+    #             # print("trainIdx: " + str(matches[i].trainIdx))
+    #             # queryImg_idx = matches[i].queryIdx
+    #             # targetImg_idx = matches[i].trainIdx
+    #             #
+    #             # (x1, y1) = kpQuery[queryImg_idx].pt
+    #             # (x2, y2) = kpTarget[targetImg_idx].pt
+    #
+    #             img3 = cv2.drawMatches(targetImg, kpTarget, frame, kpQuery, matches[:i], None)
+    #             img4 = cv2.resize(img3, (0, 0), fx=0.5, fy=0.5)
+    #             cv2.imshow("Matches", img4)
+    #         print(numMatched)
+    #
+    #         #if numMatched >= 22:
+    #         #    print("matched!")
+    #
+    #
+    #
+    #         x = cv2.waitKey(20)
+    #         c = chr(x & 0xFF)
+    #         if c == 'q':
+    #             break
+    #
+    # vidCap.release()
+    # cv2.destroyAllWindows()
 
-            matches = flanner.match(desTarget, desQuery)
-            matches.sort(key=lambda x: x.distance)  # sort by distance
-
-
-            matchedKPs = {}
-            # draw matches with distance less than threshold
-            #i = 0
-            #compare each alphImgs to frame and count how many matches
-            # for img in alphImgs:
-            #     desTarget = alphFeatureDict.get(img)[1]
-            #     matches = flanner.match(desTarget, desQuery)
-            #     for i in range(len(matches)):
-            #         if matches[i].distance > 50.0:
-            #             break
-            #         print(matches[i])
-
-
-            for alph in alphFeatureDict:
-                targetImg = cv2.imread('alphabets/'+alph+'.png')
-                kpTarget = alphFeatureDict.get(alph)[0]
-                desTarget = alphFeatureDict.get(alph)[1]
-
-                matches = flanner.match(desTarget, desQuery)
-                matches.sort(key=lambda x: x.distance)  # sort by distance
-                numMatched = 0
-                for i in range(len(matches)):
-                    if matches[i].distance > 50.0:
-                        break
-                    numMatched += 1
-                matchedKPs.update({alph: numMatched})
-
-                # print("matches[i]: "+str(matches[i]), end=" ")
-                # print("distance: "+str(matches[i].distance), end=" ")
-                # print("imgIdx: "+str(matches[i].imgIdx), end=" ")
-                # print("queryIdx: " + str(matches[i].queryIdx), end=" ")
-                # print("trainIdx: " + str(matches[i].trainIdx))
-                # queryImg_idx = matches[i].queryIdx
-                # targetImg_idx = matches[i].trainIdx
-                #
-                # (x1, y1) = kpQuery[queryImg_idx].pt
-                # (x2, y2) = kpTarget[targetImg_idx].pt
-
-                img3 = cv2.drawMatches(targetImg, kpTarget, frame, kpQuery, matches[:i], None)
-                img4 = cv2.resize(img3, (0, 0), fx=0.5, fy=0.5)
-                cv2.imshow("Matches", img4)
-            # print(numMatched)
-
-            #if numMatched >= 22:
-            #    print("matched!")
-
-
-
-            x = cv2.waitKey(20)
-            c = chr(x & 0xFF)
-            if c == 'q':
-                break
-
-    vidCap.release()
-    cv2.destroyAllWindows()
-    """
-    frame = cv2.imread('Door.png')
+    frame = cv2.imread('letterSamples/hatch.png')
     kpQuery, desQuery = computeORB(frame)
 
     matchedKPs = {}
@@ -282,14 +285,14 @@ if __name__ == '__main__':
             numMatched += 1
         matchedKPs.update({letter: numMatched})
 
-        img3 = cv2.drawMatches(targetImg, kpTarget, frame, kpQuery, matches[:i], None)
-        img4 = cv2.resize(img3, (0, 0), fx=0.5, fy=0.5)
-        cv2.imshow("Matches", img4)
-        x = cv2.waitKey(20)
-        c = chr(x & 0xFF)
-        if c == 'q':
-            break
-    cv2.destroyAllWindows()
+    #     img3 = cv2.drawMatches(targetImg, kpTarget, frame, kpQuery, matches[:i], None)
+    #     img4 = cv2.resize(img3, (0, 0), fx=0.5, fy=0.5)
+    #     cv2.imshow("Matches", img4)
+    #     x = cv2.waitKey(20)
+    #     c = chr(x & 0xFF)
+    #     if c == 'q':
+    #         break
+    # cv2.destroyAllWindows()
 
     # print(matchedKPs)
     d_sorted_by_value = OrderedDict(sorted(matchedKPs.items(), key=lambda x: x[1]))
